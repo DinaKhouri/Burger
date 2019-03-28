e6ed92e82d4f2dd083c5ebf316ec4cd3f17c983f
Dina Khouri <dina.khouri.91@gmail.com>
2019-03-28 12:48:45 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
-
+var connection;
 if (process.env.JAWSDB_URL) {
-  connecntion = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connecntion = mysql.createConnection({
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
