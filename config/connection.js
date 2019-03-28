ea6e87741e2ba2f0cf27a85c63ed87fa695736ad
Dina Khouri <dina.khouri.91@gmail.com>
2019-03-28 12:00:13 -0500

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,24 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "d1i2n3a4",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connecntion = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connecntion = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "d1i2n3a4",
+    database: "burgers_db"
+  });
+}
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "d1i2n3a4",
+//   database: "burgers_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
